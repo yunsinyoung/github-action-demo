5092c66c6e06405264b3ff1e5077f9046248a993
sinyoung <sinyoung@ex-em.com>
2024-02-08 16:03:47 +0900

    [Configuration] test 11 commit

--- src/main/java/com/demo/githubactiondemo/controller/HomeController.java
+++ src/main/java/com/demo/githubactiondemo/controller/HomeController.java
@@ -13,6 +13,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello CICD! version: " + version + " 10th Commit";
+        return "Hello CICD! version: " + version + " 11th Commit";
     }
 }
